7631d5a0455f7a473d89e1cd4a96df6e2c5ae6f7
Richard Cho <richardcho100@gmail.com>
2018-05-17 23:55:01 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,22 @@
 class Owner
-  # code goes here
+  attr_accessor :pets
+  @@all = []
+
+
+  def initialize(name)
+    @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def self.reset_all
+    @@all.size
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.all
+    @@all
+  end
 end

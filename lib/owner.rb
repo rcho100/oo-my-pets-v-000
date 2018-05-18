ff737f1edb4696f40ec9dc4927c6b6770d64fb48
Richard Cho <richardcho100@gmail.com>
2018-05-18 01:27:16 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -57,7 +57,7 @@ class Owner
   end
 
   def sell_pets
-
+    @pets = {fishes: [], cats: [], dogs: []}
   end
 
   # * When an owner buys a new pet, the `buy_``cat/dog/fish` method takes in an argument of a *name*. You must take that name and do the following:

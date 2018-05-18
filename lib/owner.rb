b6aa652034daace9ddb5d52e3755c4a8e6c20694
Richard Cho <richardcho100@gmail.com>
2018-05-18 00:24:52 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -26,4 +26,16 @@ class Owner
   def say_species
     "I am a #{self.species}."
   end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+  end
+
+  # * When an owner buys a new pet, the `buy_``cat/dog/fish` method takes in an argument of a *name*. You must take that name and do the following:
+  #   * *Make a new instance of the appropriate pet, initializing it with that name*.
+  #   * Associate that new pet instance to the owner by adding it to the appropriate array-value of the `@pets` hash stored in the `pets` `attr_accessor`.
+  # * When an owner plays with a cat or feeds a fish or walks a dog, that pet will get happier. Remember that the `pets` method stores all of an owners pets.
+  # The `@pets` hash stored in that method is full of *instances of the `Cat`/`Dog`/`Fish` class*.
+  # That means you can call `Cat`/`Dog`/`Fish` instance methods (such as `.mood=`) on those pets.
+
 end

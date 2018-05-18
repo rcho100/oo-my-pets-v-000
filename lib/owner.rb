b203840c9fce105816235dd1ec355eba8f71b2d0
Richard Cho <richardcho100@gmail.com>
2018-05-18 00:30:55 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,8 +30,8 @@ class Owner
   def buy_fish(name)
     fish = Fish.new(name)
     array_of_pets = self.pets
-     
-    # binding.pry
+
+     binding.pry
   end
 
   # * When an owner buys a new pet, the `buy_``cat/dog/fish` method takes in an argument of a *name*. You must take that name and do the following:

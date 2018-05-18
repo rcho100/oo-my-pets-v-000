f2863c53f7e6f577e7de975aa9fd72c039e7d3c2
Richard Cho <richardcho100@gmail.com>
2018-05-18 00:57:30 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -30,8 +30,8 @@ class Owner
   def buy_fish(name)
     fish = Fish.new(name)
     pets_hash = self.pets
-    pets_hash[:fishes] << fish
     binding.pry
+    pets_hash[:fishes] << fish
   end
 
   # * When an owner buys a new pet, the `buy_``cat/dog/fish` method takes in an argument of a *name*. You must take that name and do the following:

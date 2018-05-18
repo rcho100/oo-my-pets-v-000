3f7e5696b3a1d321d5f5973ef1f1b30fcb36dcc7
Richard Cho <richardcho100@gmail.com>
2018-05-18 01:37:51 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -62,7 +62,7 @@ class Owner
   end
 
   def list_pets
-
+    self.pets
   end
 
   # * When an owner buys a new pet, the `buy_``cat/dog/fish` method takes in an argument of a *name*. You must take that name and do the following:
